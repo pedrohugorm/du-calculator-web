963ac9f7f78e3782153c601070482b5045b407f7
Pedro Matos <pedrohugorm@gmail.com>
2023-07-29 20:29:00 -0700

    UI Feedback and Bound variables can be edited

---
+++ DUCalculator.Web/Domain/AppState.cs
@@ -0,0 +1,17 @@
+﻿using DUCalculator.Web.Domain.Common;
+using DUCalculator.Web.Domain.WeaponDamage;
+
+namespace DUCalculator.Web.Domain;
+
+public static class AppState
+{
+    public static event Action OnUpdate;
+
+    public static void NotifyUpdated()
+    {
+        OnUpdate?.Invoke();
+    }
+    
+    public static WeaponDamageSubProgram Program = new(new WebConsoleOutputWriter());
+    public static WeaponDamageContext Context => Program.Context;
+}
