1433dff37b809ea5b54497c976b52e5ace4966c2
Pedro Matos <pedrohugorm@gmail.com>
2024-03-29 09:54:42 -0700

    Hex Ring Generator Working

--- DUCalculator.Web/Domain/HexGrid/OffsetBasedHexGridGenerator.cs
+++ DUCalculator.Web/Domain/HexGrid/OffsetBasedHexGridGenerator.cs
@@ -9,47 +9,110 @@ public class OffsetBasedHexGridGenerator : IHexGridGenerator
     public IHexGridGenerator.Result GenerateGrid(IHexGridGenerator.Settings settings)
     {
         const int suInKm = 200000;
-        
+
         var baseHexRings = HexRingGenerator.GenerateRings(
             new HexRingGenerator.HexRingOptions(
                 RingCount: settings.NumRings
             )
         ).Select(p => p * suInKm);
 
-        var start = settings.EndPosition.PositionToVector3();
-        var end = settings.StartPosition.PositionToVector3();
+        var start = settings.StartPosition.PositionToVector3();
+        var end = settings.EndPosition.PositionToVector3();
 
         var direction = Vector3.Normalize(end - start);
         var distance = Math.Abs(Vector3.Distance(end, start));
 
         var rotatedStartRings = baseHexRings
-            .Select(v => RotateVectorToMatchNormal(v, direction))
-            .Select(v => v + start)
+            .Select(v => RotateVectorToMatchNormal(v, direction) + start)
             .ToArray();
 
         var rotatedEndRings = rotatedStartRings
             .Select(v => v + direction * distance)
             .ToArray();
 
-        var wplList = new List<IHexGridGenerator.WaypointLine>();
-
+        var waypointMap = new Dictionary<string, IHexGridGenerator.Waypoint>();
         for (var i = 0; i < rotatedStartRings.Length; i++)
         {
-            var posA = rotatedStartRings[i];
-            var posB = rotatedEndRings[i];
+            var name = $"{i + 1}A";
+            waypointMap.Add(
+                name,
+                new IHexGridGenerator.Waypoint(name, rotatedStartRings[i].Vector3ToPosition(), "A")
+            );
+        }
 
+        for (var i = 0; i < rotatedEndRings.Length; i++)
+        {
+            var name = $"{i + 1}B";
+            waypointMap.Add(
+                name,
+                new IHexGridGenerator.Waypoint(name, rotatedEndRings[i].Vector3ToPosition(), "B")
+            );
+        }
+
+        var wplList = new List<IHexGridGenerator.WaypointLine>
+        {
+            new(
+                waypointMap["1A"],
+                waypointMap["1B"]
+            )
+        };
+
+        var currentId = 1;
+        var previousType = "A";
+        var currentType = "B";
+
+        var typeMap = new Dictionary<int, string>
+        {
+            { 0, "A" },
+            { 1, "B" }
+        };
+        var typeVal = 1;
+
+        var orderedWaypoints = new Queue<IHexGridGenerator.Waypoint>();
+        
+        for (;;)
+        {
+            var different = previousType != currentType;
+
+            if (different)
+            {
+                currentId++;
+            }
+            else
+            {
+                typeVal = (typeVal + 1) % 2;
+            }
+            
+            previousType = currentType;
+            currentType = typeMap[typeVal];
+
+            if (!waypointMap.TryGetValue($"{currentId}{currentType}", out var wp))
+            {
+                break;
+            }
+            
+            orderedWaypoints.Enqueue(wp);
+        }
+
+        while (orderedWaypoints.Any())
+        {
             wplList.Add(
                 new IHexGridGenerator.WaypointLine(
-                    new IHexGridGenerator.Waypoint($"{i + 1}A", posA.Vector3ToPosition(), "A"),
-                    new IHexGridGenerator.Waypoint($"{i + 1}B", posB.Vector3ToPosition(), "B")
-                )
+                    orderedWaypoints.Dequeue(),
+                    orderedWaypoints.Dequeue()
+                ).Reversed(settings.ReverseOrder)
             );
+        }
+
+        if (settings.ReverseOrder)
+        {
+            wplList.Reverse();
         }
 
         return new IHexGridGenerator.Result(
             wplList,
             default,
-            wplList.Count(),
+            wplList.Count,
             default,
             default,
             default
@@ -59,7 +122,7 @@ public class OffsetBasedHexGridGenerator : IHexGridGenerator
     private static Vector3 RotateVectorToMatchNormal(Vector3 vector, Vector3 normal)
     {
         var forward = new Vector3(1, 0, 0);
-        
+
         var rotationAxis = Vector3.Cross(forward, normal)
             .NormalizeSafe();
         var rotationAngle = (float)Math.Acos(
