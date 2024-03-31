535ece92e8f29642349e580881d7a4ede15b8d48
Pedro Matos <pedrohugorm@gmail.com>
2024-03-30 18:42:12 -0700

    Added Clipboard reader for quicker copying and deduplicator of positions

---
+++ DUCalculator.Web/Scripts/Clipboard.ps1
@@ -0,0 +1,29 @@
+﻿# Path to the file where clipboard content will be appended
+$file = "clipboard_content.txt"
+
+$lastValue = ""
+$counter = 1
+
+# Continuously monitor the clipboard and append new content to the file
+while ($true) {
+    # Get clipboard content
+    $clipboardContent = Get-Clipboard -Format Text
+
+    # Check if clipboard content is not empty
+    if ($null -ne $clipboardContent) {
+        # Append clipboard content to the file  
+
+        if($lastValue -ne $clipboardContent)
+        {
+            $lastValue = $clipboardContent
+
+            Add-Content -Path $file -Value "WP$counter $clipboardContent"
+            Write-Host "$clipboardContent"
+
+            $counter++
+        }
+    }
+
+    # Wait for some time before checking the clipboard again
+    Start-Sleep -Seconds 1  # Adjust the delay as needed
+}
